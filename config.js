83f359e54408cec01841da70f907e28b80aa1c13
moshiyoshicodes <yashgirdhar2001@gmail.com>
2026-02-09 17:15:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you, Navsa Arora be my cutue lil Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you, Navya Arora be my cutue lil Valentine on February 14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
